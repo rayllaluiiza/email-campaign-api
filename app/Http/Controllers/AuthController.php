31c62a6a5f885e0f714fba3ee7ad192a7042ff31
Raylla <raylla_op12@hotmail.com>
2026-05-27 21:10:07 -0300

    feat: adicionar endpoint de registro com retorno de token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function register()
+    public function register(RegisterRequest $request)
     {
-
+        $user = User::create($request->validated());
+        return response()->json(['token' => $user->createToken('api')->plainTextToken], 201);
     }
 
     public function login()
