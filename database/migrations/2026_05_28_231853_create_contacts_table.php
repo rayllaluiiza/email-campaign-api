ad489e3b923a458f413bc05827276123ec6da416
Raylla <raylla_op12@hotmail.com>
2026-05-28 20:22:45 -0300

    feat: criar migration da tabela de contatos

---
+++ database/migrations/2026_05_28_231853_create_contacts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('contact_list_id')->constrained();
+            $table->string('name', 100);
+            $table->string('email', 100);
+            $table->timestamps();
+
+            $table->unique(['contact_list_id', 'email']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
