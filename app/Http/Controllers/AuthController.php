e8e96e4bf2f4d15650607fe4eef9cb8fcef655ee
Raylla <raylla_op12@hotmail.com>
2026-05-27 22:36:28 -0300

    feat: adicionar login e logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -14,13 +15,21 @@ class AuthController extends Controller
         return response()->json(['token' => $user->createToken('api')->plainTextToken], 201);
     }
 
-    public function login()
+    public function login(Request $request)
     {
-        
+        $credentials = $request->only('email', 'password');
+
+        if(!Auth::attempt($credentials)){
+            return response()->json(['message' => 'Credenciais inválidas.'], 401);
+        }
+
+        $token = Auth::user()->createToken('api')->plainTextToken;
+        return response()->json(['token' => $token], 200);
     }
 
-    public function logout()
+    public function logout(Request $request)
     {
-        
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'Logout realizado.']);
     }
 }
