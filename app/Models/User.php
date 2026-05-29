f583283f79ae95515763201be86ff4239d2922df
Raylla <raylla_op12@hotmail.com>
2026-05-28 21:02:45 -0300

    feat: adicionar relacionamento da tabela user

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,4 +30,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function contactLists()
+    {
+        return $this->hasMany(ContactList::class);
+    }
 }
